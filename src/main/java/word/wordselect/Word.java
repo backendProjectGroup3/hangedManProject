bc0eb4b880b0852b3e8ae9812e1d49b4feeaacda
DESKTOP-6NEOP34\SAMSUNG <ehdejdrnfl@naver.com>
2024-03-25 10:53:58 +0900

    hint and word

--- src/main/java/word/wordselect/Word.java
+++ src/main/java/word/wordselect/Word.java
@@ -1,15 +1,15 @@
 package word.wordselect;
 
 public class Word {
-    public static void main(String[] args) {
+    public static String WordHardEasy (String difficulty){
         String[] words = {"apple", "banana", "orange", "watermelon", "pineapple", "grape",
-                "strawberry", "blueberry", "blackberry", "cantaloupe", "kiwifruit",
-                "peach", "plum", "pear", "cherry", "melon", "fig", "date", "kiwi",
-                "avocado", "coconut", "lime", "mango", "papaya", "lychee", "durian",
-                "apricot", "tangerine", "cranberry", "nectarine", "guava", "grapefruit",
-                 "dragonfruit", "persimmon", "pomegranate", "rhubarb",
-               "jackfruit", "lemon", "lime", "mango", "banana", "peach", "plum",
-                "pear", "cherry", "grape","Serendipity","Ephemeral","Obfuscate"};
+            "strawberry", "blueberry", "blackberry", "cantaloupe", "kiwifruit",
+            "peach", "plum", "pear", "cherry", "melon", "fig", "date", "kiwi",
+            "avocado", "coconut", "lime", "mango", "papaya", "lychee", "durian",
+            "apricot", "tangerine", "cranberry", "nectarine", "guava", "grapefruit",
+            "dragonfruit", "persimmon", "pomegranate", "rhubarb",
+            "jackfruit", "lemon", "lime", "mango", "banana", "peach", "plum",
+            "pear", "cherry", "grape","Serendipity","Ephemeral","Obfuscate"};
 
         String[] longWords = new String[words.length];
         String[] shortWords = new String[words.length];
@@ -38,7 +38,21 @@ public class Word {
         System.out.println("6<WORD");
         for (int i = 0; i < shortIndex; i++) {
             System.out.print(shortWords[i]+",");
+
         }
+        int su = (int)(Math.random()*longIndex);
+        int su2 = (int)(Math.random()*shortIndex);
+
+        if ("easy".equals(difficulty)) {
+            return longWords[su];
+        } else if ("hard".equals(difficulty)) {
+            return shortWords[su2];
+        } else {
+            // 예외 처리: 매개변수가 "easy"나 "hard"가 아닌 경우
+            return null;
+        }
+
+
 
     }
 
