b580e8de95fd31ef876d26794312586e434427e8
DESKTOP-QELH4GD\솔혜 <blackdemon1@naver.com>
2024-03-25 15:58:12 +0900

    수정된 포인트. 1. 단어 종류 추가.

--- src/main/java/com/hmproject/wordselect/Word.java
+++ src/main/java/com/hmproject/wordselect/Word.java
@@ -10,7 +10,8 @@ public class Word {
                 "apricot", "tangerine", "cranberry", "nectarine", "guava", "grapefruit",
                 "boysenberry", "dragonfruit", "persimmon", "pomegranate", "rhubarb",
                 "jackfruit", "lemon", "lime", "mango", "Serendipity", "Ephemeral",
-                "Obfuscate"};
+                "Obfuscate", "cooperation","conscience","application","cilantro","elect",
+                "adaptable","handiwork", "bathe","query", "omniscient","blame"};
 
     }
 
